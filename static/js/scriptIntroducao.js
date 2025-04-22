a1886720d3c5d737dbcdeb06dac3be30ecf18e02
Carlos Icaro <carlos.paiva@sou.inteli.edu.br>
2025-04-22 18:38:34 -0300

    feat: Navegação funcional entre as 3 primeiras telas

---
+++ static/js/scriptIntroducao.js
@@ -0,0 +1,34 @@
+let container = document.querySelector('.container');
+let nextButtons = document.querySelectorAll('.next');
+let items = document.querySelectorAll('.item');
+
+let active = 0;
+
+nextButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        console.log("Botão clicado");
+
+        // Verifica se o botão clicado é o último
+        if (active === items.length - 1) {
+            active = 0; // Reinicia o contador
+            console.log("Último botão clicado");
+            window.location.href = "dashboard"
+            return; // Interrompe a execução do restante do código
+        }
+
+        // Retira a classe active do item atual
+        let itemOld = container.querySelector('.list .item.active');
+        if (itemOld) {
+            itemOld.classList.remove('active');
+        }
+
+        // Incrementa a variável active
+        active++;
+
+        // Adiciona a classe active ao próximo item
+        let itemNew = container.querySelector('.list .item:nth-child(' + (active + 1) + ')');
+        if (itemNew) {
+            itemNew.classList.add('active');
+        }
+    });
+});
